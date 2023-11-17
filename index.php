dd7bdf8f32135d9ac4c7e131f3133562def9380f
Yassine Bouaquil <139541292+yassine-bouaquil@users.noreply.github.com>
2023-11-17 12:09:25 +0100

    commit

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <!DOCTYPE html>
-<<<<<<< HEAD
     <html>
 		<link rel="stylsheet" href="style.css">
     <head>
@@ -47,39 +46,3 @@
     	</div>
     </body>
     </html>
-=======
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>Formulaire d'Annonce</title>
-</head>
-
-<body>
-    <h1>Bonjour !!Veuillez Enter Votre Produit </h1>
-
-    <form action="YASINE.php" method="post" enctype="multipart/form-data">
-        <label for="nom">Nom :</label>
-        <input type="text" id="nom" name="nom" required>
-
-        <label for="prenom">Prénom :</label>
-        <input type="text" id="prenom" name="prenom" required>
-
-        <label for="image">Image :</label>
-        <input type="file" id="image" name="image" accept="image/*" required>
-
-        <label for="date">Date :</label>
-        <input type="date" id="date" name="date" required>
-
-        <label for="description">Description :</label>
-        <textarea id="description" name="description" rows="4" required></textarea>
-
-        <input type="submit" value="Soumettre">
-    </form>
-
-</body>
-
-</html>
->>>>>>> f84888402653459f84f889287df8587235689d0d
